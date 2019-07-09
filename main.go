c780c30c8f8f1ffe1edffea62f8f863ca58ad621
Andrew Booth <booth@shipt.com>
2019-07-09 10:28:10 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	dir := flag.String("d", ".", "dir")
+	port := flag.String("p", "8080", "port")
+	flag.Parse()
+
+	log := log.New(os.Stdout, "[fileserver] ", log.LstdFlags)
+
+	log.Printf("fileserver running @ :%s\n", *port)
+	log.Panic(http.ListenAndServe(":"+*port, loggerMiddleware(log)(http.FileServer(http.Dir(*dir)))))
+}
+
+func loggerMiddleware(log *log.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			recorder := &statusRecorder{
+				ResponseWriter: w,
+				statusCode: http.StatusOK,
+			}
+
+			before := time.Now()
+			next.ServeHTTP(w, r)
+			elapsed := time.Since(before)
+
+			log.Printf("%s %s%s | %v | %d \n", r.Method, r.URL.Path, r.URL.RawQuery, elapsed, recorder.statusCode)
+		})
+	}
+}
+
+type statusRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (r *statusRecorder) WriteHeader(statusCode int) {
+	r.statusCode = statusCode
+	r.ResponseWriter.WriteHeader(statusCode)
+}
